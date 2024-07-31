2562377204e3c4481096da532df840aeb7eddccc
Rafik Petrosyan <rafpetro@c2r17s1.42yerevan.am>
2024-07-31 17:13:45 +0400

    Task 3 completed

--- philo/philos.c
+++ philo/philos.c
@@ -6,7 +6,7 @@
 /*   By: rafpetro <rafpetro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 16:24:17 by rafpetro          #+#    #+#             */
-/*   Updated: 2024/07/31 16:24:18 by rafpetro         ###   ########.fr       */
+/*   Updated: 2024/07/31 16:44:30 by rafpetro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	main(int argc, char **argv)
 {
 	if (!(argc == 5 || argc == 6))
-		exit(write(2, "Invalid Argument !\n", 19));
+		exit(write(2, "Invalid number of arguments\n", 28));
 	validacia(argc, argv);
 }
